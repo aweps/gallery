b4dfaa61520358c9ae0f77803672e664d5e6a372
genx7up <genx7up@users.noreply.github.com>
2020-08-16 01:02:00 +0000

    Web test

--- _ops/release-all.sh
+++ _ops/release-all.sh
@@ -3,4 +3,4 @@
 set -Eeuo pipefail
 
 flutter build web --dart-define=${DART_DEFINES:-}
-flutter build apk --dart-define=${DART_DEFINES:-}
+flutter build apk --dart-define=${DART_DEFINES:-} || :
--- _ops/run.tests.sh
+++ _ops/run.tests.sh
@@ -6,4 +6,6 @@ source _ops/get-deps.sh
 
 dartanalyzer ./ --fatal-infos --fatal-warnings
 
+flutter analyze
 flutter test
+flutter pub run grinder verify-code-segments
