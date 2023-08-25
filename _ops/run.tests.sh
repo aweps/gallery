5da8c18aced7f8224ebd7633bc6e1e87c972bc08
gman <genx7up@users.noreply.github.com>
2023-08-25 16:19:15 -0700

    [ci skip] fixes

--- _ops/run.tests.sh
+++ _ops/run.tests.sh
@@ -14,7 +14,7 @@ fi
 flutter pub get
 flutter doctor -v
 
-dart analyze ./ --fatal-infos --fatal-warnings
+dart analyze ./ --fatal-warnings
 
 if [[ "${DEBUG:-}" == "true" ]]; then VERBOSE_FLAG="-v"; fi
 flutter test ${VERBOSE_FLAG:-} --no-pub ${DART_DEFINES:-}
